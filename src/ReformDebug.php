eb30f464f84f3f21a2ff2f73ee1ebf827fb75cee
Andrey Tyurin <ectb08@mail.ru>
2019-08-02 00:18:14 +0300

    add debug method first 1

---
+++ src/ReformDebug.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace Oploshka\Reform;
+
+class Reform implements \Oploshka\Reform\ReformInterface {
+  
+  private $reformMethods = [
+    'string'        => 'Oploshka\\ReformItem\\StringReform'       ,
+    'int'           => 'Oploshka\\ReformItem\\IntReform'          ,
+    'float'         => 'Oploshka\\ReformItem\\FloatReform'        ,
+    'email'         => 'Oploshka\\ReformItem\\EmailReform'        ,
+    'password'      => 'Oploshka\\ReformItem\\PasswordReform'     ,
+    'origin'        => 'Oploshka\\ReformItem\\OriginReform'       ,
+    'datetime'      => 'Oploshka\\ReformItem\\DateTimeReform'     ,
+    'json'          => 'Oploshka\\ReformItem\\JsonReform'         ,
+    'objToJson'     => 'Oploshka\\ReformItem\\ObjToJsonReform'   ,
+    
+    'array'         => 'Oploshka\\ReformItem\\ArrayReform'        ,
+    'simpleArray'   => 'Oploshka\\ReformItem\\SimpleArrayReform'  ,
+  ];
+
+  function  __construct($reformMethods = []){
+    $this->reformMethods = array_merge($this->reformMethods, $reformMethods);
+    foreach ($this->reformMethods as $key => $val){
+      if($val === false){
+        unset($this->reformMethods[$key]);
+      }
+    }
+  }
+  
+  /*
+   * Функция валидации переменной
+   */
+  public function item($item = null, $validate = array() ){
+    // проверим данные на существование
+    if( $item === null )                { return null; }
+    if( !is_array($validate) )          { return null; }
+    if( !isset($validate['type']) )     { return null; }
+    if( !is_string($validate['type']) ) { return null; }
+    if(!isset($this->reformMethods[ $validate['type'] ])){ return null; }
+    //
+    $ValidateClassName = $this->reformMethods[ $validate['type'] ];
+    $useClass = new $ValidateClassName();
+    // проверим реализует ли класс наш интерфейс
+    if ( !($useClass instanceof \Oploshka\Reform\ReformItemInterface ) ) { return null; }
+  
+    $_validate = $validate['validate'] ?? [];
+    return $useClass::validate($item, $_validate, $this);
+  }
+  
+}
---
+++ test/ReformDebugTest.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace Oploshka\Reform;
+
+use PHPUnit\Framework\TestCase;
+
+class ReformTest extends TestCase {
+
+  public function testReformWork() {
+
+    $Reform = new \Oploshka\Reform\Reform();
+
+    // test item string
+    $this->assertTrue( $Reform->item('string' , ['type' => 'string']) === 'string');
+    $this->assertTrue( $Reform->item(123456   , ['type' => 'int']   ) === 123456  );
+    $this->assertTrue( $Reform->item(1234.56  , ['type' => 'float'] ) === 1234.56 );
+    $this->assertTrue( $Reform->item(1234.56  , ['type' => 'float'] ) === 1234.56 );
+    $this->assertTrue( $Reform->item('email@mail.ru', ['type' => 'email'] ) === 'email@mail.ru' );
+    $this->assertTrue( $Reform->item('Password_#1'  , ['type' => 'password'] ) !== null  );
+    $this->assertTrue( $Reform->item('{"s":"a"}'    , ['type' => 'origin'] ) === '{"s":"a"}' );
+    $this->assertTrue( $Reform->item('{"s":4}'      , ['type' => 'json'] ) !== null );
+    $this->assertTrue( $Reform->item('{"s":4}'      , ['type' => 'json'] )['s'] === 4 );
+    $this->assertEquals( $Reform->item(["s"=>4], ['type' => 'objToJson'] ), '{"s":4}' );
+
+    // test array
+    $reformArray1 = [ 's' => 'test string', 'i' => 23, ];
+    $reformArray1Schema = [
+      'type' => 'array',
+      'validate' => [
+        's' => ['type' => 'string' ],
+        'i' => ['type' => 'int'    ],
+      ],
+    ];
+    $reformArray1Result = $Reform->item($reformArray1, $reformArray1Schema);
+    $this->assertEquals($reformArray1Result, $reformArray1);
+  
+    $reformArray2 = [ 'a' => ['i'=> 24, 's' => 'string 2',], ];
+    $reformArray2Schema = [
+      'type' => 'array',
+      'validate' => [
+        'a' => [
+          'type' => 'array',
+          'validate' => [
+            'i' => ['type' => 'int'    ],
+            's' => ['type' => 'string' ],
+          ],
+        ],
+      ],
+    ];
+    $reformArray2Result = $Reform->item($reformArray2, $reformArray2Schema);
+    $this->assertEquals($reformArray2Result, $reformArray2);
+  
+    
+    // test simple array
+    $reformSimpleArray1 = [ 1, 85, '49', 76 ];
+    $reformSimpleArray1Schema = [
+      'type' => 'simpleArray',
+      'validate' => [ 'type' => 'int' ],
+    ];
+    $reformSimpleArray1Result = $Reform->item($reformSimpleArray1, $reformSimpleArray1Schema);
+    $this->assertEquals($reformSimpleArray1Result, $reformSimpleArray1);
+    
+    $reformSimpleArray1 = [ 1, 'string', 49, 76 ];
+    $reformSimpleArray1Schema = [
+      'type' => 'simpleArray',
+      'validate' => [ 'type' => 'int' ],
+    ];
+    $reformSimpleArray1Result = $Reform->item($reformSimpleArray1, $reformSimpleArray1Schema);
+    $this->assertEquals($reformSimpleArray1Result, null);
+  }
+}
